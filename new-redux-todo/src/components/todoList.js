a381de9d5ee5def76af2cdf14c1d99f95f7ab8c7
Kathryn Hampton <khampton119@gmail.com>
2019-01-28 21:31:36 -0500

    starting a new todo project

---
+++ new-redux-todo/src/components/todoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>this is my todo list</h3>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
